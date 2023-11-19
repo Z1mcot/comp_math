5b660bb39855f8f5c376ea8259d917be15740d5c
Dyubko <dyubko.r@digdes.com>
2023-11-19 12:53:04 +0300

    feat: add SOR method

---
+++ 1_2_3/1_2_3.py
@@ -0,0 +1,64 @@
+import json
+import os
+import numpy as np
+
+# # Определение системы уравнений Ax = f
+# A = np.array([[4, -1, 0],
+#               [-1, 4, -1],
+#               [0, -1, 3]])
+# f = np.array([5, -4, 7])
+
+# Функция, реализующая метод последовательных верхних релаксаций (метод Зейделя)
+def sor_method(A, f, gamma=1.2, initial_guess=None, max_iterations=100, tolerance=1e-6):
+    if initial_guess is None:
+        x = np.zeros_like(f)
+    else:
+        x = initial_guess
+    
+    for _ in range(max_iterations):
+        x_new = x.copy()
+        for i in range(A.shape[0]):
+            s1 = np.dot(A[i, :i], x_new[:i])
+            s2 = np.dot(A[i, i+1:], x[i+1:])
+            x_new[i] = (1 - gamma)*x[i] + (gamma / A[i, i])*(f[i] - s1 - s2)
+        
+        if np.allclose(x, x_new, atol=tolerance, rtol=0):
+            return x_new
+        
+        x = x_new
+    
+    return x
+
+# # Выполнение метода последовательных верхних релаксаций для системы уравнений
+# solution = sor_method(A, f, gamma=1.2, initial_guess=None, max_iterations=100, tolerance=1e-6)
+# print("Решение системы уравнений:", solution)
+
+def calc_residual_matrix(A, X, F):
+    return F - np.matmul(A, X) # где matmul - матричное умножение    
+
+# Для красивого вывода ответа
+def round_array(arr, digits = 6):
+    rounded_arr = np.round(arr, digits)
+    return rounded_arr
+
+def main():
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    # Считываем данные из файла
+    with open(f'{root_dir}/matrices.json', 'r') as file:
+        data = json.load(file)
+
+    A_matrices = data['A_matrices']
+    f = np.array(data['f'])
+
+    for i in range(len(A_matrices)):
+        A = np.array(A_matrices[i])
+        print(f"Для исходных данный под номерами {i+1} и 6:\n")
+
+        X = sor_method(A.copy(), f.copy(), gamma=1.2, initial_guess=None, max_iterations=100, tolerance=1e-6)
+        print("Решение:\n", X)
+
+        residual = calc_residual_matrix(A, X, f)
+        print("Матрица невязки:\n", residual)
+
+if __name__ == '__main__':
+    main()
